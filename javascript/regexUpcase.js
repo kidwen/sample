61d4d35f222d7165671158126f69a9d3142c9a05
kidwen <xuqiujia2020@outlook.com>
2021-08-24 00:01:33 +0800

    add note to regex file

--- javascript/regexUpcase.js
+++ javascript/regexUpcase.js
@@ -1,6 +1,12 @@
 'use strict';
 
-function upper(all, letter) {
+/**
+ *
+ * @param {string} _ - the regex match string
+ * @param {string} letter - the regex catch string
+ * @returns {string} the regex catch string to camel case
+ */
+function upper(_, letter) {
     return letter.toUpperCase();
 }
 
