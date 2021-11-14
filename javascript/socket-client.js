735ada0e9021e5de0e64f0535c0e6227b674f46e
kidwen <xuqiujia2020@outlook.com>
2021-11-15 00:06:04 +0800

    socket client

--- javascript/socket-client.js
+++ javascript/socket-client.js
@@ -1,15 +1,32 @@
-(function () {
-    const net = require('net');
-    const socket = net.createConnection({ host: '127.0.0.1', port: 3000 }, () => {
-        console.log('createConnection success');
-    });
-    socket.connect();
-    socket.emit('events', { test: 'test' })
-    socket.addListener('events', event => {
-        console.log('events', event);
-    })
-    // socket('write')
 
+(function () {
+    // const net = require('net');
+    // const socket = net.createConnection({ host: '127.0.0.1', port: 3000 }, () => {
+    //     console.log('createConnection success');
+    // });
+    // socket.connect();
+    // socket.emit('events', { test: 'test' })
+    // socket.addListener('events', event => {
+    //     console.log('events', event);
+    // })
+    // socket('write');
+
+    const io = require('socket.io-client');
+    const socket = io('ws://localhost:3000');
+    socket.on('connect', () => {
+        // either with send()
+        // socket.send('events');
+
+        // or with emit() and custom event names
+        socket.emit('events', 'Hello!', { 'mr': 'john' }, Uint8Array.from([1, 2, 3, 4]));
+    });
+    socket.on('event', (log) => {
+        console.log('on welcome : welcome received renderer', log); // displayed
+        // socket.emit('events')
+        // socket.close();
+        // socket.disconnect();
+    });
+    //'ws://localhost:3000'
 
     // const socket = new WebSocket('http://localhost:3000');
 
