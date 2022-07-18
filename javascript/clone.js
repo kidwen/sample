b78da8769eeaea5b9ab26b3111bd67db93e41b50
kidwen <xuqiujia2020@outlook.com>
2022-07-18 10:41:45 +0800

    Create clone.js

---
+++ javascript/clone.js
@@ -0,0 +1,13 @@
+function clone (value) {
+  if (Array.isArray(value)) {
+    return value.map(clone)
+  } else if (value && typeof value === 'object') {
+    const res = {}
+    for (const key in value) {
+      res[key] = clone(value[key])
+    }
+    return res
+  } else {
+    return value
+  }
+}
