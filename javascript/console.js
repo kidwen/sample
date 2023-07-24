ca8ecffc375d0b08cd4c9d350a71b3c10fbdc5ff
kidwen <xuqiujia2020@outlook.com>
2023-07-24 16:34:49 +0800

    Update console.js

--- javascript/console.js
+++ javascript/console.js
@@ -8,3 +8,11 @@ console.log(String.raw`%c
 ██║  ██╗██║██████╔╝╚███╔███╔╝███████╗██║ ╚████║
 ╚═╝  ╚═╝╚═╝╚═════╝  ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝
 `, 'color: #6156f4;');
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fdcf8134374198bc3cb685263f563c46";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
